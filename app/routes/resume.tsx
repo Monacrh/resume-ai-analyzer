4412a6d87a7bf182859d925a0e84f0b479b8b4d5
Monarch <bryandhaniel123@gmail.com>
2025-09-19 05:35:43 +0700

    feat: add Accordion and Details components

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -92,6 +92,8 @@ import {useEffect, useState} from "react";
 import {usePuterStore} from "~/lib/puter";
 // import Summary from "~/components/Summary";
 import ATS from "~/components/ATS";
+import Details from "~/components/Details";
+import Summary from "~/components/Summary";
 // import Details from "~/components/Details";
 
 export const meta = () => ([
@@ -164,9 +166,9 @@ const Resume = () => {
                     <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
                     {feedback ? (
                         <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-                            {/* <Summary feedback={feedback} /> */}
+                            <Summary feedback={feedback} />
                             <ATS score={feedback.ATS.score || 0} suggestions={feedback.ATS.tips || []} />
-                            {/* <Details feedback={feedback} /> */}
+                            <Details feedback={feedback} />
                         </div>
                     ) : (
                         <img src="/images/resume-scan-2.gif" className="w-full" />
